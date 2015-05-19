a14f5a7189fcbfd930eafb0d2dbb4af7f35eeac6
Luca Bruzzone <spam@unsigned.it>
2015-05-19 23:46:13 +0200

    implemented navbar left custom

---
+++ app/src/main/java/it/unsigned/weatherappturin/NavigationDrawerFragment.java
@@ -0,0 +1,25 @@
+package it.unsigned.weatherappturin;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class NavigationDrawerFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+
+    public NavigationDrawerFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+    }
+
+}
